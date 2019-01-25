555b63df1bc9bec449af54089b15231281742d6b
ldc4 <ldc4@vip.qq.com>
2019-01-25 17:36:30 +0800

    添加动画库react-spring

--- pages/knowledge.js
+++ pages/knowledge.js
@@ -1,51 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { Transition, animated } from 'react-spring';
+
+const imgs = [
+  style => (
+    <animated.img style={{ width: '100%', height: '100%', ...style }} src={`/static/roadmap-zh.png`} alt="知识地图(中文版)" />
+  ),
+  style => (
+    <animated.img style={{ width: '100%', height: '100%', ...style }} src={`/static/roadmap-en.png`} alt="Knowledge(English Version)" />
+  ),
+];
 
 class KnowledgePage extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      radioValue: 'zh',
+      index: 0,
     };
   }
 
-  handleRadioChange = (e) => {
-    const { value } = e.target;
-    this.setState({ radioValue: value });
+  toggle = (e) => {
+    this.setState(state => ({
+      index: state.index === imgs.length - 1 ? 0 : state.index + 1,
+    }));
   }
 
   render() {
-    const { radioValue } = this.state;
+    const { index } = this.state;
     return (
-      <div className="container">
+      <div className="container" onClick={this.toggle}>
         <style jsx>{`
           .container {
             max-width: 1024px;
             margin: 0 auto;
+            overflow: hidden;
           }
         `}</style>
-        <img style={{ width: '100%', height: '100%' }} src={`/static/roadmap-${radioValue}.png`} alt="知识地图(中文版)" />
+        <Transition
+          native
+          reset
+          unique
+          items={index}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
+        >
+          {index => imgs[index]}
+        </Transition>
         <div>
-          <label>
-            <input
-              type="radio"
-              name="lang"
-              value="zh"
-              checked={radioValue === 'zh'}
-              onChange={this.handleRadioChange}
-            />
-            <span>中文</span>
-          </label>
-          <label style={{ marginLeft: 10 }}>
-            <input
-              type="radio"
-              name="lang"
-              value="en"
-              checked={radioValue === 'en'}
-              onChange={this.handleRadioChange}
-            />
-            <span>English</span>
-          </label>
+          <span>点击图片切换语言</span>
           <span style={{ float: 'right' }}>引用至（https://github.com/adam-golab/react-developer-roadmap）</span>
         </div>
       </div>
